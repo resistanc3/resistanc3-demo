ab107b302c07d639f87db3955bf86ca898062a6b
Jenny <jmartinez414@gmail.com>
2022-05-28 03:22:58 +0000

    header

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,9 @@ import { ConnectButton } from "web3uikit"
 export default function Header() {
     return (
         <nav className="p-5 border-b-2 flex flex-row">
+            <button className="navbar-toggler" data-toggle="collapse" data-target="#navLinks" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
             <h1 className="py-4 px-4 font-bold text-3xl"> The Resistanc3 DApp</h1>
             <div className="ml-auto py-2 px-4">
                 <ConnectButton moralisAuth={false}/>
