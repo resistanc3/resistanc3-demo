aa90e9cb58b088159fd478c77c441522b2e8d1c9
Jenny <jmartinez414@gmail.com>
2022-05-28 03:38:47 +0000

    nav

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,16 @@ export default function Header() {
             <button className="navbar-toggler" data-toggle="collapse" data-target="#navLinks" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
+            <div className="collapse navbar-collapse justify-content-center" id="navLinks">
+                <ul className="navbar-nav">
+                    <li className="navbar-nav">
+                        <a href="" className="nav-link">Home</a>
+                    </li>
+                    <li className="nav-item px-4">
+                        <a href="" className="nav-link">How it works</a>
+                    </li>
+                </ul>
+            </div>
             <h1 className="py-4 px-4 font-bold text-3xl"> The Resistanc3 DApp</h1>
             <div className="ml-auto py-2 px-4">
                 <ConnectButton moralisAuth={false}/>
